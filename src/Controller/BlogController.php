b21c5433e4401a54bf7a359fc45eac8c96c45e6b
ezra <ezra@techtribe.nl>
2019-03-08 09:58:24 +0100

    finised section 4.17

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,28 +12,40 @@ use App\Service\Greeting;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class BlogController extends AbstractController
+/**
+ * @Route("/blog")
+ * Class BlogController
+ * @package App\Controller
+ */
+class BlogController
 {
 
     /**
      * @var Greeting
      */
     private $greeting;
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
 
-    public function __construct(Greeting $greeting)
+    public function __construct(Greeting $greeting, \Twig_Environment $twig)
     {
         $this->greeting = $greeting;
+        $this->twig = $twig;
     }
 
     /**
-     * @Route("/", name="blog_index")
+     * @Route("/{name}", name="blog_index")
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(Request $request)
+    public function index($name)
     {
-        return $this->render('base.html.twig', ['message' => $this->greeting->greet($request->get('name'))]);
+        $html = $this->twig->render('base.html.twig', ['message' => $this->greeting->greet($name)]);
+        return new Response($html);
 
     }
 
